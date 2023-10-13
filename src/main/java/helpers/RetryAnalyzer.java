1a86a30c06365c5b82aa0fde9ad93003fbdb169e
laganin <laganin23@live.com>
2023-10-13 10:09:14 +0200

    add retry analyzer

---
+++ src/main/java/helpers/RetryAnalyzer.java
@@ -0,0 +1,19 @@
+package helpers;
+
+import org.testng.IRetryAnalyzer;
+import org.testng.ITestResult;
+
+public class RetryAnalyzer implements IRetryAnalyzer {
+
+    private int count = 0;
+    private int maxRetry = 3;
+
+    @Override
+    public boolean retry(ITestResult iTestResult) {
+        if (count < maxRetry) {
+            count++;
+            return true;
+        }
+        return false;
+    }
+}
