4ebcb57386bd7208a7bcbb1d099f0359152deeca
laganin <laganin23@live.com>
2023-10-18 22:57:01 +0200

    add Auth Routes Test Forbids vistis to home url if not authenticated

---
+++ src/test/java/tests/AuthRoutesTests.java
@@ -0,0 +1,15 @@
+package tests;
+
+import helpers.RetryAnalyzer;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.testng.annotations.Test;
+
+public class AuthRoutesTests extends BasicTest {
+    @Test(priority = 1, retryAnalyzer = RetryAnalyzer.class)
+    public void forbidsVisitsToHomeUrlIfNotAuthenticated() {
+        driver.navigate().to(baseUrl + "/home");
+        wait
+                .withMessage("URL does not contain /login")
+                .until(ExpectedConditions.urlContains("/login"));
+    }
+}
